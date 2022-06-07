2fed2f813cb5d4fffdd010a24a634f124622bb1b
Roman <roma20.94.a@gmail.com>
2022-06-07 16:24:32 +0300

    init commit

--- database/migrations/2022_05_30_000000_update_users_table.php
+++ database/migrations/2022_05_30_000000_update_users_table.php
@@ -27,6 +27,9 @@ class UpdateUsersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+            $table->dropColumn('phone_number');
+        });
     }
 }
